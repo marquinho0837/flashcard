94068c4c845dcd00a35327a711a9a4ceb1fa082e
marquinho0837 <marco.moura.santos@escola.pr.gov.br>
2025-05-30 09:25:49 -0300

    .,

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,26 @@ criaCartao(
     'Em que ano o Brasil proclamou sua independência?',
     '1822'
 )
+
+criaCartao(
+    'História',
+    'Em que ano o Brasil proclamou sua independência?',
+    '1822'
+)
+
+criaCartao(
+    'História',
+    'Em que ano o Brasil proclamou sua independência?',
+    '1822'
+    )
+
+criaCartao(
+    'História',
+    'Em que ano o Brasil proclamou sua independência?',
+    '1822'
+    )
+
+criaCartao(
+    'História',
+    'Em que ano o Brasil proclamou sua independência?',
+    '1822'
